8c6fdc7cec79caec54ada8d3a6eb03c8341d5b55
Zvezdobroeca <jginev@gmail.com>
2022-03-14 14:26:01 +0200

    adding fifth and final problem for this section

---
+++ PYTHON FUNDAMENTALS/1. BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS/05. how_much_coffee_do_you_need.py
@@ -0,0 +1,18 @@
+events = ('cat', 'dog', 'coding', 'movie')
+coffee_cups = 0
+
+while True:
+    command = input()
+    if command == "END":
+        break
+    else:
+        if command.lower() in events:
+            if command.isupper():
+                coffee_cups += 2
+            elif command.islower():
+                coffee_cups += 1
+
+if coffee_cups > 5:
+    print("You need extra sleep")
+else:
+    print(coffee_cups)
