e9c19d8f8af596fa7ceb8a991dc616fe0fefbde2
Zvezdobroeca <jginev@gmail.com>
2022-03-16 08:18:33 +0200

    fixed a typo

--- PYTHON FUNDAMENTALS/03. LISTS BASICS/02. messaging.py
+++ PYTHON FUNDAMENTALS/03. LISTS BASICS/02. messaging.py
@@ -10,7 +10,7 @@ for index in numbers:
     while index > len(message):  # if index is out of range
         index -= len(message)   # we continue counting from start
 
-    # add the char at index number to decoded
+    # add the char at that index to decoded
     decoded += message[index]
     # remove the char from the original string
     message = message[:index] + message[index+1:]
