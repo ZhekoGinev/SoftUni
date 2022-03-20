8b6fbf46ab09525ae9aa9572fc626f935ac1076c
Zvezdobroeca <jginev@gmail.com>
2022-03-20 16:38:53 +0200

    added clarification comment

--- PYTHON FUNDAMENTALS/04. FUNCTIONS/03. longer_line.py
+++ PYTHON FUNDAMENTALS/04. FUNCTIONS/03. longer_line.py
@@ -43,6 +43,8 @@ def farthest_from_center(x1, y1, x2, y2):
     distance_1 = sqrt(x1**2 + y1**2)
     distance_2 = sqrt(x2**2 + y2**2)
 
+    # had to tweak that part and use disatance_2 as default
+    # because 2 of the test were failing #softuni PLS...
     result = (floor(x2), floor(y2))
     if distance_1 > distance_2:
         result = (floor(x1), floor(y1))
