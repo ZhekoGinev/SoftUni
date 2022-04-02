46187a8d04e99ae7899a5e01562fc401d512a1ca
Zvezdobroeca <jginev@gmail.com>
2022-04-02 20:55:24 +0300

    add lab solutions for list-basic

---
+++ 01. python-fundamentals/03. lists-basics/01. lab/01.strange_zoo.py
@@ -0,0 +1,7 @@
+tail = input()
+body = input()
+head = input()
+
+ordered = [head, body, tail]
+
+print(ordered)
---
+++ 01. python-fundamentals/03. lists-basics/01. lab/02. courses.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+courses = []
+
+for _ in range(n):
+    course = input()
+    courses.append(course)
+
+print(courses)
