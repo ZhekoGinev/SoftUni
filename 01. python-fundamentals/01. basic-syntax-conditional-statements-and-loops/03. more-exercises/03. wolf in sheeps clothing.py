ea659bacbcbe5e7c547e6b1f4d13139ed13d5450
Zvezdobroeca <jginev@gmail.com>
2022-04-05 08:49:11 +0300

    minor update

--- 01. python-fundamentals/01. basic-syntax-conditional-statements-and-loops/03. more-exercises/03. wolf in sheeps clothing.py
+++
@@ -1,8 +0,0 @@
-herd = input().split(", ")
-herd.reverse()
-if herd[0] == "wolf":
-    print("Please go away and stop eating my sheep")
-else:
-    next_sheep = herd.index("wolf")
-    print(f"Oi! Sheep number {next_sheep}! " +
-          "You are about to be eaten by a wolf!")
