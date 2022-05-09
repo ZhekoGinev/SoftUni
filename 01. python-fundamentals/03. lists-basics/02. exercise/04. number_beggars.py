a6448d93322816d4c8763f20bcdbe286283469b0
Zvezdobroeca <jginev@gmail.com>
2022-05-09 15:41:28 +0300

    minor refactor

--- 01. python-fundamentals/03. lists-basics/02. exercise/04. number_beggars.py
+++ 01. python-fundamentals/03. lists-basics/02. exercise/04. number_beggars.py
@@ -1,13 +1,11 @@
 money = [int(num) for num in input().split(", ")]
 number_of_beggars = int(input())
 # generate a list of beggars, each one with 0 as initial value
-beggars_profit = [0 for _ in range(number_of_beggars)]
+beggars_profit = [0] * number_of_beggars
 
 while len(money) > 0:
     for beggar in range(number_of_beggars):
         if len(money) > 0:
             beggars_profit[beggar] += money.pop(0)
-        else:
-            beggars_profit[beggar] += 0
 
 print(beggars_profit)
