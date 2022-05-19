70ff82733946f356b2af364fc6984d3bced12686
Zvezdobroeca <jginev@gmail.com>
2022-05-19 10:56:04 +0300

    fix names in fundamentals / data types / lab, no changes in problems

--- 01-python-fundamentals/02-data-types-and-variables/01-lab/03-special-numbers.py
+++ 01-python-fundamentals/02-data-types-and-variables/01-lab/03-special-numbers.py
@@ -4,8 +4,8 @@ for number in range(1, boundary + 1):
     numstring = str(number)
     sum = 0
     is_special = False
-    for d in numstring:
-        sum += int(d)
+    for digit in numstring:
+        sum += int(digit)
     if sum in (5, 7, 11):
         is_special = True
     print(f"{number} -> {is_special}")
