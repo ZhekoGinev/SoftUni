45e84d6fc57f347892ef3f41fad1877c1c32f558
Zvezdobroeca <jginev@gmail.com>
2022-05-21 19:51:35 +0300

    add solutions 6-7 for fundanmentals/data-types/exercise

---
+++ 01-python-fundamentals/02-data-types-and-variables/02-exercise/07-water-overflow.py
@@ -0,0 +1,12 @@
+CAPACITY = 255
+number_of_fills = int(input())
+filled = 0
+
+for fill in range(number_of_fills):
+    water = int(input())
+    if water + filled > CAPACITY:
+        print("Insufficient capacity!")
+        continue
+    else:
+        filled += water
+print(filled)
