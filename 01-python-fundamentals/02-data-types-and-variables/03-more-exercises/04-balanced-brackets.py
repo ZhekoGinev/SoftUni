edc904e136b224dcf6b2e1c7e2c9bbb1c77a46c6
Zvezdobroeca <jginev@gmail.com>
2022-05-29 17:15:01 +0300

    minor refactor change var names

--- 01-python-fundamentals/02-data-types-and-variables/03-more-exercises/04-balanced-brackets.py
+++ 01-python-fundamentals/02-data-types-and-variables/03-more-exercises/04-balanced-brackets.py
@@ -1,19 +1,17 @@
 number_of_lines = int(input())
-
-data = ""
-
+brackets = ""
 is_balanced = False
 
-# I only care about the brackets
+# we only care about the brackets
 for _ in range(number_of_lines):
-    ui = input()
-    if ui == "(" or ui == ")":
-        data += ui
+    data = input()
+    if data == "(" or data == ")":
+        brackets += data
 
 # nested brackets are considered unbalanced
 # so it becomes a very easy check
-for i in range(0, len(data), 2):
-    if data[i] == "(" and data[i + 1] == ")":
+for i in range(0, len(brackets), 2):
+    if brackets[i] == "(" and brackets[i + 1] == ")":
         is_balanced = True
     else:
         is_balanced = False
