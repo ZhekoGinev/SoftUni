e33ffd219b340d3eaaed543985b9b3366b892043
Zvezdobroeca <jginev@gmail.com>
2022-05-30 17:18:46 +0300

    add solution 1-2 for functions/exercise

---
+++ 01-python-fundamentals/04-functions/02-exercise/01-smallest-of-three-numbers.py
@@ -0,0 +1,9 @@
+def smallest(x, y, z):
+    return min(x, y, z)
+
+
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+
+print(smallest(first_num, second_num, third_num))
