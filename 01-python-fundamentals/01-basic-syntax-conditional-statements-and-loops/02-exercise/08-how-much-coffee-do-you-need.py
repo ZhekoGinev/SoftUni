3cad79e9d72e3a838b0b65c0bf9334bc365e2303
Zvezdobroeca <jginev@gmail.com>
2022-06-10 19:04:51 +0300

    minor refactor

--- 01-python-fundamentals/01-basic-syntax-conditional-statements-and-loops/02-exercise/08-how-much-coffee-do-you-need.py
+++ 01-python-fundamentals/01-basic-syntax-conditional-statements-and-loops/02-exercise/08-how-much-coffee-do-you-need.py
@@ -5,12 +5,11 @@ while True:
     command = input()
     if command == "END":
         break
-    else:
-        if command.lower() in events:
-            if command.isupper():
-                coffee_cups += 2
-            elif command.islower():
-                coffee_cups += 1
+    if command.lower() in events:
+        if command.isupper():
+            coffee_cups += 2
+        elif command.islower():
+            coffee_cups += 1
 
 if coffee_cups > 5:
     print("You need extra sleep")
