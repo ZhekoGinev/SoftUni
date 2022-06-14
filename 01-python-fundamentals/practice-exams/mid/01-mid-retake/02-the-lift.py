d4429cb7902458a30b66d65b2d7cf1dfa3c6cd01
Zvezdobroeca <jginev@gmail.com>
2022-06-14 14:33:54 +0300

    add solution the-lift for practice-exams/mid

---
+++ 01-python-fundamentals/practice-exams/mid/01-mid-retake/02-the-lift.py
@@ -0,0 +1,22 @@
+people = [1] * int(input())
+lifts = [int(i) for i in input().split()]
+max_capacity = len(lifts) * 4
+
+for i, _ in enumerate(lifts):
+    while lifts[i] < 4 and people:
+        lifts[i] += people.pop()
+    if not people:
+        break
+
+current_capacity = sum(lifts)
+
+lifts = ' '.join(str(s) for s in lifts)
+
+if people:
+    print(f"There isn't enough space! {len(people)} people in a queue!")
+    print(lifts)
+elif max_capacity > current_capacity:
+    print("The lift has empty spots!")
+    print(lifts)
+elif max_capacity == current_capacity:
+    print(lifts)
