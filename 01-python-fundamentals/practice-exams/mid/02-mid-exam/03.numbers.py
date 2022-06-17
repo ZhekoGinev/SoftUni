ef5b06fe2929440fbc4946a048e1334bdd8f6214
Zvezdobroeca <jginev@gmail.com>
2022-06-17 19:14:42 +0300

    minor refactor

--- 01-python-fundamentals/practice-exams/mid/02-mid-exam/03.numbers.py
+++ 01-python-fundamentals/practice-exams/mid/02-mid-exam/03.numbers.py
@@ -2,7 +2,7 @@ nums = sorted([int(i) for i in input().split()], reverse=True)
 avg = sum(nums) / len(nums)
 result = [n for n in nums if n > avg][:5]
 
-if len(nums) > 1 and result:
+if result:
     print(*result)
 else:
     print("No")
