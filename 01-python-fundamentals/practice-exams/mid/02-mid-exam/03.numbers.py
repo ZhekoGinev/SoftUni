8d939a9802406ae36b9b9dcf9e556bbf4ee29af2
Zvezdobroeca <jginev@gmail.com>
2022-06-17 19:01:43 +0300

    minor refactor (removed set check)

--- 01-python-fundamentals/practice-exams/mid/02-mid-exam/03.numbers.py
+++ 01-python-fundamentals/practice-exams/mid/02-mid-exam/03.numbers.py
@@ -2,7 +2,7 @@ nums = sorted([int(i) for i in input().split()], reverse=True)
 avg = sum(nums) / len(nums)
 result = [n for n in nums if n > avg][:5]
 
-if len(nums) > 1 and len(set(nums)) > 1:
-    print(*result, sep=" ")
+if len(nums) > 1 and result:
+    print(*result)
 else:
     print("No")
