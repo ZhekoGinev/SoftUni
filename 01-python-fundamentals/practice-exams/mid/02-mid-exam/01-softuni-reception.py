b6e1f9d51737745cd255045c4eb7bd6478764f9e
Zvezdobroeca <jginev@gmail.com>
2022-06-17 19:21:39 +0300

    removed second solution

--- 01-python-fundamentals/practice-exams/mid/02-mid-exam/01-softuni-reception.py
+++ 01-python-fundamentals/practice-exams/mid/02-mid-exam/01-softuni-reception.py
@@ -1,5 +1,3 @@
-### simple solution to beat the judge tests
-
 emplyees_capacities = sum([int(input()), int(input()), int(input())])
 students_count = int(input())
 hours = 0
@@ -11,31 +9,3 @@ while students_count > 0:
     students_count -= emplyees_capacities
 
 print(f"Time needed: {hours}h.")
-
-
-
-### second solution which is much more accurate logically speaking
-### it can be made to calculate to the minute as well as various
-### other options and cannot have a negative students count
-
-
-# from copy import deepcopy
-
-# emplyees_capacities = [int(input()), int(input()), int(input())]
-# students_count = int(input())
-# hours = 0
-
-# while students_count > 0:
-#     hours += 1
-#     if hours % 4 == 0:
-#         continue
-#     emps = deepcopy(emplyees_capacities)
-#     while students_count > 0 and sum(emps) > 0:
-#         for index, emp_cpty in enumerate(emps):
-#             if emp_cpty > 0:
-#                 students_count -= 1
-#                 emps[index] -= 1
-#             if students_count == 0:
-#                 break
-
-# print(f"Time needed: {hours}h.")
