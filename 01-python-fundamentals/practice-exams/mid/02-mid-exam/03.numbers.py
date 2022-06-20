c1f3fe29ba5eb5259cf3b1341e459eac4a12d9b4
Zvezdobroeca <jginev@gmail.com>
2022-06-20 16:59:30 +0300

    temporary omitted some solutions due to upcoming exams, solutions will be visible again after June 27th

--- 01-python-fundamentals/practice-exams/mid/02-mid-exam/03.numbers.py
+++
@@ -1,8 +0,0 @@
-nums = sorted([int(i) for i in input().split()], reverse=True)
-avg = sum(nums) / len(nums)
-result = [n for n in nums if n > avg][:5]
-
-if result:
-    print(*result)
-else:
-    print("No")
