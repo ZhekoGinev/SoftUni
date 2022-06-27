845bd92ee00c9780b60be850532e43a73e97a736
Zvezdobroeca <jginev@gmail.com>
2022-06-27 05:56:52 +0300

    refactor the movement algorithm

--- Python/01-python-fundamentals/05-lists-advanced/03-more-exercises/03-kates-way-out.py
+++ Python/01-python-fundamentals/05-lists-advanced/03-more-exercises/03-kates-way-out.py
@@ -1,6 +1,6 @@
 n_of_rows = int(input())
 can_escape = True
-
+moves = 1
 # init matrix
 matrix = []
 for _ in range(n_of_rows):
@@ -10,42 +10,44 @@ for _ in range(n_of_rows):
 for i, row in enumerate(matrix):
     for j, col in enumerate(row):
         if col == "k":
-            position = [i, j]
+            pos = [i, j]
             break
-
-# track visited squares
-visited = [position]
 
 while True:
 
-    row = position[0]
-    col = position[1]
+    row = pos[0]
+    col = pos[1]
 
     if not can_escape:
         print("Kate cannot get out")
         break
 
     # check adjacent squares and move if empty
-    if matrix[row - 1][col] == " " and [row - 1, col] not in visited:  # up
-        position = [row - 1, col]
-    elif matrix[row + 1][col] == " " and [row + 1, col] not in visited:  # down
-        position = [row + 1, col]
-    elif matrix[row][col - 1] == " " and [row, col - 1] not in visited:  # left
-        position = [row, col - 1]
-    elif matrix[row][col + 1] == " " and [row, col + 1] not in visited:  # right
-        position = [row, col + 1]
+    if matrix[row - 1][col] == " " and [row - 1, col]:  # up
+        pos = [row - 1, col]
+        matrix[pos[0]][pos[1]] = "#"
+        moves += 1
+    elif matrix[row + 1][col] == " " and [row + 1, col]:  # down
+        pos = [row + 1, col]
+        matrix[pos[0]][pos[1]] = "#"
+        moves += 1
+    elif matrix[row][col - 1] == " " and [row, col - 1]:  # left
+        pos = [row, col - 1]
+        matrix[pos[0]][pos[1]] = "#"
+        moves += 1
+    elif matrix[row][col + 1] == " " and [row, col + 1]:  # right
+        pos = [row, col + 1]
+        matrix[pos[0]][pos[1]] = "#"
+        moves += 1
 
     else:
         can_escape = False
 
-    if can_escape:  # keep track of visited squares
-        visited.append([row, col])
-
     if (  # if we reach an exit break the loop
-        position[0] == 0
-        or position[0] == len(matrix) - 1
-        or position[1] == 0
-        or position[1] == len(matrix[row]) - 1
+        pos[0] == 0
+        or pos[0] == len(matrix) - 1
+        or pos[1] == 0
+        or pos[1] == len(matrix[row]) - 1
     ):
-        print(f"Kate got out in {len(visited)} moves")
+        print(f"Kate got out in {moves} moves")
         break
