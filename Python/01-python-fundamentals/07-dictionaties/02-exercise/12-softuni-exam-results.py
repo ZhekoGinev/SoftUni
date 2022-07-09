d97490ac7b64bc1a8201f94aa7c8830516d0e744
Zvezdobroeca <jginev@gmail.com>
2022-07-09 07:23:24 +0300

    add solution to softuni-exam-results

---
+++ Python/01-python-fundamentals/07-dictionaties/02-exercise/12-softuni-exam-results.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+results = defaultdict(list)
+submissions = defaultdict(int)
+
+while True:
+    data = input()
+    if data == "exam finished":
+        break
+
+    tokens = data.split("-")
+    username = tokens[0]
+    language = tokens[1]
+    points = tokens[-1]
+
+    if username in results and language == "banned":
+        results.pop(username)
+    elif language != "banned":
+        results[username].append(int(points))
+        submissions[language] += 1
+
+print("Results:")
+for username, result in results.items():
+    print(f"{username} | {max(result)}")
+
+print("Submissions:")
+for language, count in submissions.items():
+    print(f"{language} - {count}")
